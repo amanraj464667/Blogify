1f189c6b7e12206e96c385cc31c9271814cd826e
Aman <rajaman@gmail.com>
2025-05-16 17:27:58 +0530

    second commit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,11 +4,11 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Rishabh' Blogify
+            About Aman Blogify
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Rishabh's Blogify! This blog was created by Rishabh 
+              Welcome to Aman Blogify! This blog was created by Aman 
               as a personal project to share his thoughts and ideas with the
               world. Rishabh is a passionate developer who loves to write about
               technology, coding, and everything in between.
@@ -17,7 +17,7 @@ export default function About() {
             <p>
               On this blog, you'll find various articles on topics
               such as web development, software engineering, and programming
-              languages. Rishabh is always learning and exploring new
+              languages. Aman is always learning and exploring new
               technologies, so be sure to check back often for new content!
             </p>
 
